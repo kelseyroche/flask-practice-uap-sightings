059ca5fa6b643c9e0dc116980067520cbe390dd2
Kelsey Roche <roche.kelsey.24@gmail.com>
2025-01-08 15:48:42 -0500

    lab part 1

--- server/models.py
+++ server/models.py
@@ -1,5 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
 
 # Initialize Flask-SQLAlchemy
+db = SQLAlchemy()
 
 # Define the Sighting model
+class Sighting(db.Model):
+    __tablename__ = "sightings"
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String)
+    time = db.Column(db.String)
+    location = db.Column(db.String)
+    shape_of_craft = db.Column(db.String)
+    approximate_size = db.Column(db.Integer)
+    approximate_speed = db.Column(db.Integer)
+    description = db.Column(db.String)
+    reporter = db.Column(db.String)
+    reporter_reliable_witness = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f'<Sighting {self.id}, {self.location}>'
--- server/seed.py
+++ server/seed.py
@@ -1,9 +1,35 @@
 # import app
+from app import app
 # import model and db instance
+from models import db, Sighting
+
+from faker import Faker
+import random
 
 # Define seeding functions (optional: use Faker to help generate fake data)
+shapes = ['round','square','strange','confusing']
+
+fake = Faker()
+
+def seed():
+    sightings = []
+    for _ in range(0,5):
+        sights = Sighting(
+            date=fake.date("%m-%d-%Y"),
+            time=fake.time(),
+            location=fake.address(),
+            shape_of_craft=random.choice(shapes),
+            approximate_size=random.randint(10,1000),
+            approximate_speed=random.randint(5,500),
+            description=fake.text(40),
+            reporter=fake.name(),
+            reporter_reliable_witness=fake.boolean()
+        )
+        sightings.append(sights)
+   
+    db.session.add_all(sightings)
+    db.session.commit()
 
 if __name__ == "__main__":
     with app.app_context():
-        # Call seeding functions
-        pass
+        seed()
